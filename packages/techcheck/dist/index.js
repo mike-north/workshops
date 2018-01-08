6765a66c9f658d6a4e8e76ce0233b705a884fa66
Michael North <michael.l.north@gmail.com>
2018-01-07 20:37:22 -0800

    new build

--- packages/techcheck/dist/index.js
+++ packages/techcheck/dist/index.js
@@ -38,9 +38,10 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, new Promise(function (res, rej) {
+                case 0: return [4 /*yield*/, new Promise(function (res) {
                         setTimeout(function () {
                             process.stdout.write('Checking some other things');
+                            res();
                         }, 2000);
                     })];
                 case 1:
